140535e3b475acc230e40becbfa4480a09ad044f
Sourav Dutt <sdutt12139@gmail.com>
2024-09-26 12:57:18 +0530

    remove default/breeze auth views ui

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,10 +12,10 @@
             </ul>
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('login') }}">Sign in</a>
+                    <a class="nav-link" aria-current="page" href="{{-- {{ route('login') }} --}}">Sign in</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="{{ route('register') }}">Sign up</a>
+                    <a class="nav-link" aria-current="page" href="{{-- {{ route('register') }} --}}">Sign up</a>
                 </li>
             </ul>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,6 @@ use App\Http\Controllers\ProfileController;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
 Route::post('/generate-text', [AIController::class, 'generateText'])->name('generate-text');
 Route::post('/summarize-text', [AIController::class, 'summarizeText'])->name('summarize-text');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
